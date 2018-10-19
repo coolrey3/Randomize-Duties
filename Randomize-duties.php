582e3ceeb45aadd2ce542aabe18dfbd15e4870f7
coolrey3 <coolrey3@gmail.com>
2018-10-19 04:42:02 -0400

    added original file

---
+++ Randomize-duties.php
@@ -0,0 +1,27 @@
+
+
+<?php
+$tasks = array("Trash", "Windows", "Counters");
+$employees  = array("John","Ray","West","Garrett");
+
+echo "<h1>Closing Tasks</h1>";
+
+/**
+
+            <tr>
+                <th>
+                    Task
+                    Employee
+                <th>
+                </th>
+                </th>";
+                </th> 
+                </th>
+       <tr><td>" . $employees[array_rand($employees)] . "</td> </tr>"} ." </table> ";
+        echo "<tr><td>" . $employees[array_rand($employees)] . "</td> </tr>"} ." </table> ";
+
+                        } ;
+
+
+?>
+
