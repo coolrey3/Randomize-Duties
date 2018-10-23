d1a580d59708bd7c8fa8641f29302b3d10f4f8af
Ray Mercedes <contact@RayMercedes.com>
2018-10-23 16:31:48 -0400

    changed from html to php

--- index.php
+++ index.php
@@ -14,10 +14,10 @@
 </head>
 
 <body>
-    
+
     <!--Header-->
 
-    
+
     <div style='width:100%;text-align:center'>
         <h1 style='box-shadow:inset 1px 1px rgba(255,255,255,0.1), 1px 1px rgba(0,0,0,0.1);
                border-style: solid;border-color: black;border-width:.5px;
@@ -25,15 +25,15 @@
             Daily Assignments
         </h1>
     </div>
-    
+
     <!--Instructions-->
-    
-    
+
+
     <h3 style='text-align:center'>Select the employees to assign tasks to.</h3>
-    
+
     <!--checkmarks-->
-    
-    
+
+
     <div style='text-align:center;margin-top:-5px;'>
         <?php
         $employees  = array("John","Ray","West","Garrett",);
@@ -42,17 +42,17 @@
         };
         ?>
     </div>
-    
+
     <!--Button-->
-    
-    
+
+
     <div style='margin:auto;text-align:center;'>
         <br><input type='submit' value='Generate' style='margin:auto'><br><br>
     </div>
-    
+
     <!--Table-->
-    
-    
+
+
     <table style='height:43%;border-style:solid;width:90%;text-align:center;background-color:gray;margin:auto;'>
         <tr style='border-style:solid;'>
             <th style='color:red;background-color:white;border-style:solid;border-color:black;'>
@@ -62,8 +62,8 @@
                 Employee
             </th>
         </tr>
-    
-    <?php
+
+        <?php
         ####Variables#######
         
         
@@ -85,5 +85,6 @@
                 #############footer info##########
                 include('footer.php');
         ?>
-        </body>
-    </html>
+</body>
+
+</html>
