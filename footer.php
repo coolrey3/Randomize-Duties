9251ef6ca5f00eb84cc810c3f2c6a18ab9a1d9e0
Ray Mercedes <contact@RayMercedes.com>
2018-10-23 00:37:42 -0400

    Update footer.php
    fixed footer website link

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 #############footer info##########
 echo "<style> a {color:white;}</style>";
 $footer=array("<a href='http://start.mobilerepairtechs.com'>POS</a>",
-              "<a href='http://www.mobilerepairtechs.com/'>Website Front End</a>" ,
+              "<a href='http://www.cellphonerepair.com/gainesville-fl'>Website Front End</a>" ,
               "<a href='http://www.mobilerepairtechs.com/MRTbovada2018' >Website Back End</a>",
               "<a href='http://facebook.com/mobilerepairtechs'>Facebook</a>",
               "<a href='http://task.mobilerepairtechs.com'>Tasks</a>",
