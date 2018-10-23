693104cdc1a04850866f0ef5dfbde3add7986dc6
Ray Mercedes <contact@RayMercedes.com>
2018-10-22 23:33:58 -0400

    syntax fix

--- tasks.php
+++ tasks.php
@@ -60,7 +60,7 @@ foreach($task as $task) {
                               $task
                          </td>" .
                          "<td style='background-color:white;border-style:solid;color:black'>" . 
-                              $employees[array_rand(employees)] . 
+                              $employees[array_rand($employees)] . 
                           "</td> 
                      </tr> " ;
                    
